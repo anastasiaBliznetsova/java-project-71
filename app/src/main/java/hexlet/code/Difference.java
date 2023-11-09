76b7dbec9391243b450cfdf34594a035e458270b
anastasiaBliznetsova <bliznecovanastasia@gmail.com>
2023-11-09 21:35:43 +0600

    corrected with null 6

--- app/src/main/java/hexlet/code/Difference.java
+++ app/src/main/java/hexlet/code/Difference.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.TreeSet;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-
+import java.util.Objects;
 
 public class Difference {
     public static List<Map<String, Object>> filling(Map<String, Object> mapOne, Map<String, Object> mapTwo) {
@@ -14,24 +14,24 @@ public class Difference {
         List<Map<String, Object>> resultList = new ArrayList<>();
         for (String key : keySet) {
             Map<String, Object> resultMap = new LinkedHashMap<>();
-            if (!mapTwo.containsKey(key) && mapOne.containsKey(key)) {
+            if (!mapTwo.containsKey(key)) {
                 resultMap.put("event", "removed");
                 resultMap.put("key", key);
-                resultMap.put("value", mapOne.get(key));
-            } else if (!mapOne.containsKey(key) && mapTwo.containsKey(key)) {
+                resultMap.put("value", mapOne.getOrDefault(key, "null"));
+            } else if (!mapOne.containsKey(key)) {
                 resultMap.put("event", "added");
                 resultMap.put("key", key);
                 resultMap.put("value", mapTwo.get(key));
-            } else if (mapOne.containsKey(key) && mapTwo.containsKey(key)
-                    && !isEquals(mapOne.get(key), mapTwo.get(key))) {
+            } else if (!isEquals(mapOne.getOrDefault(key, "null"),
+                    mapTwo.getOrDefault(key, "null"))) {
                 resultMap.put("event", "updated");
                 resultMap.put("key", key);
-                resultMap.put("oldValue", mapOne.get(key));
-                resultMap.put("newValue", mapTwo.get(key));
+                resultMap.put("oldValue", mapOne.getOrDefault(key, "null"));
+                resultMap.put("newValue", mapTwo.getOrDefault(key, "null"));
             } else {
                 resultMap.put("event", "not changed");
                 resultMap.put("key", key);
-                resultMap.put("value", mapOne.get(key));
+                resultMap.put("value", mapOne.getOrDefault(key, "null"));
             }
             resultList.add(resultMap);
         }
@@ -44,7 +44,7 @@ public class Difference {
         } else if (valueOne == null || valueTwo == null) {
             return false;
         } else {
-            return valueOne.equals(valueTwo);
+            return Objects.equals(valueOne, valueTwo);
         }
     }
 }
